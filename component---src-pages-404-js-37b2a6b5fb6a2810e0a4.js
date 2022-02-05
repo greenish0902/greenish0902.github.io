602bf830b06b0f97e0495e2a5a75f8565e72e612
greenish0902 <greenish0902@gmail.com>
2022-02-06 00:02:47 +0900

    Updates

---
+++ component---src-pages-404-js-37b2a6b5fb6a2810e0a4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkshiloh_blog=self.webpackChunkshiloh_blog||[]).push([[883],{429:function(t,e,l){l.r(e);var a=l(7294),n=l(8678),o=l(262);e.default=function(t){var e=t.data,l=t.location,s=e.site.siteMetadata.title;return a.createElement(n.Z,{location:l,title:s},a.createElement(o.Z,{title:"404: Not Found"}),a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-37b2a6b5fb6a2810e0a4.js.map
